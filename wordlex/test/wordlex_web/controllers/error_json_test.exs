55297693bb9a8dd407c006213a54910b1a2ed530
Caleb <caleb@sele.dev>
2026-09-04 14:04:04 -0500

    Add initial wordlex project

---
+++ wordlex/test/wordlex_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule WordlexWeb.ErrorJSONTest do
+  use WordlexWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert WordlexWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert WordlexWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
